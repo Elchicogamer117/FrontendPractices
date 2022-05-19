be855247412f20c4950eb71bb3d71a91a5b040fd
Cristian <cris18002011@hotmail.com>
2022-05-18 21:47:52 -0500

    Adding funcionality

--- figures.js
+++ figures.js
@@ -1,13 +1,13 @@
-// Square code
+
 const squarePerimeter = (side) => side * 4
 const squareArea = (side) =>  side**2
-// Triangle code
+
 const trianglePerimeter = (side1, side2, base) => side1 + side2 + base
 const triangleArea = (side1, side2, base) => {
     const s = trianglePerimeter(side1, side2, base)/2
     return Math.sqrt(s*(s - side1)*(s - side2)*(s - base)) 
 } 
-// Cicle code
+
 const pi = Math.PI
 const circleDiameter = (radio) => radio * 2
 const circlePerimeter = (radio) => {
@@ -15,3 +15,10 @@ const circlePerimeter = (radio) => {
     return  diameter * Math.PI
 } 
 const circleArea = (radio) => Math.PI * (radio**2)
+
+function calculeFigure(){
+    const input = document.getElementById("squareButton")
+    const value = input.value
+    alert(`Perimetro: ${squarePerimeter(value)}
+    Area: ${squareArea(value)}`)
+}
