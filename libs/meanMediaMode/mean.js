bacd6be4afef9c0caac95e3a1e172f3fa3dfe9ba
Cristian Flores <cris18002011@hotmail.com>
2022-05-22 15:00:03 -0500

    Mean formula implementation

--- libs/meanMediaMode/mean.js
+++ libs/meanMediaMode/mean.js
@@ -1,23 +1,7 @@
-const list = [
-    100,
-    200,
-    300,
-    500
-]
-// let sumList = 0 
-//* Solution 1 : Traditional
-// for (let i=0; i < list.length; i++){
-//     sumList = sumList + list[i]
-// }
-//* Solution 2 : Iteration
-// for (let element of list){
-//     sumList = sumList + element
-// }
-//*Solution 3 : Method reduce
-const sumList = list.reduce((preValue = 0, curValue) => preValue + curValue)
+function calculateMean (list) {
+    const sumList = list.reduce((preValue = 0, curValue) => preValue + curValue)
+    const meanList = sumList / list.length
+    return meanList
+}
 
-const meanList = sumList / list.length
-
-console.log(sumList)
-console.log(meanList)
-
+console.log(calculateMean([100,200,300,500]))
