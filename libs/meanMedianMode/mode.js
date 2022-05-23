5d0098f044856892e7e9800fe18c832200e28275
Cristian <cris18002011@hotmail.com>
2022-05-22 21:10:30 -0500

    Modal function implementation

--- libs/meanMedianMode/mode.js
+++ libs/meanMedianMode/mode.js
@@ -1,16 +1,22 @@
-// const list = [1,2,2,3,3,3,4,4,4,4,5,5,5,5,5,6,6,6,6,6,6,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,9,9]
-const list = [1,2,5,3,9,7,1,3,5,8,1,5,4,6,8,0,0,6,7,9,4,8,2,3,3,1,6,5,2,0,3,7,8]
+function calculateMode(list){
+    const objectList = {}
+    list.map((element) => {
+        if(objectList[element]){
+            objectList[element] += 1;
+        } else {
+            objectList[element] = 1;
+        }
+    })
+    const orderedList = Object.entries(objectList).sort((a,b) => b[1] - a[1])
+    const mode = orderedList[0]
 
-const objectList = {}
-list.map((element) => {
-    if(objectList[element]){
-        objectList[element] += 1;
-    } else {
-        objectList[element] = 1;
-    }
-})
+    return mode
+}
 
-const orderedList = Object.entries(objectList).sort((a,b) => b[1] - a[1])
-
-const mode = orderedList[0]
-console.log(mode)
+//* Less code, more execution time
+function mode(arr){
+    return arr.sort((a,b) =>
+        arr.filter(v => v===a).length
+        - arr.filter(v => v===b).length
+    ).pop();
+}
