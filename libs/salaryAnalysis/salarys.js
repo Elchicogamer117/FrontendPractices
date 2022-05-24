e9ccba3d0eda94c18f7dc299641a0bfc3e5896e5
Cristian <cris18002011@hotmail.com>
2022-05-24 18:30:04 -0500

    Database creation

---
+++ libs/salaryAnalysis/salarys.js
@@ -0,0 +1,98 @@
+const reach = [];
+
+reach.push({
+    name: 'Jonhson',
+    salary: 780,
+})
+reach.push({
+    name: 'Alsey',
+    salary: 660,
+})
+reach.push({
+    name: 'John',
+    salary: 1170,
+})
+reach.push({
+    name: 'Cortana',
+    salary: 4529,
+})
+reach.push({
+    name: 'Kai',
+    salary: 1250,
+})
+reach.push({
+    name: 'Vanack',
+    salary: 1340,
+})
+reach.push({
+    name: 'Riz',
+    salary: 280,
+})
+reach.push({
+    name: 'Kelly',
+    salary: 870,
+})
+reach.push({
+    name: 'Linda',
+    salary: 580,
+})
+reach.push({
+    name: 'Frederic',
+    salary: 1040,
+})
+reach.push({
+    name: 'Alice',
+    salary: 1300,
+})
+reach.push({
+    name: 'Douglas',
+    salary: 420,
+})
+reach.push({
+    name: 'Carter',
+    salary: 2590,
+})
+reach.push({
+    name: 'Kat',
+    salary: 3200,
+})
+reach.push({
+    name: 'Jun',
+    salary: 2660,
+})
+reach.push({
+    name: 'Emile',
+    salary: 2390,
+})
+reach.push({
+    name: 'Jorge',
+    salary: 520,
+})
+reach.push({
+    name: 'Noble 6',
+    salary: 3120,
+})
+reach.push({
+    name: 'Roma',
+    salary: 1430,
+})
+reach.push({
+    name: 'Nicole',
+    salary: 4580,
+})
+reach.push({
+    name: 'Mike',
+    salary: 1200,
+})
+reach.push({
+    name: 'Ralph',
+    salary: 1030,
+})
+reach.push({
+    name: 'Anton',
+    salary: 440,
+})
+reach.push({
+    name: 'Grace',
+    salary: 930,
+})
